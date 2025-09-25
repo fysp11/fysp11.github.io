4115ef6e92e060c70c622d90ff59d8242c7d3921
Felipe Mendonça <fysp11@gmail.com>
2025-09-25 13:40:42 -0300

    feat: add environment-based project and demo feature flags

--- src/server/data.ts
+++ src/server/data.ts
@@ -8,3 +8,17 @@ export const getExperiences = async () => {
   );
   return sortedEntries.map((entry: CollectionEntry<'experiences'>) => entry.data);
 };
+
+export const getProjects = async () => {
+  const enabledProjectsEnv = import.meta.env.ENABLED_PROJECTS ?? '';
+  const enabledSlugs = enabledProjectsEnv.split(',')
+  const projects = await getCollection('projects');
+  let activeProjects = projects.filter((p: CollectionEntry<'projects'>) => {
+    const fileEnabled = p.data.active;
+    const envEnabled = enabledSlugs.includes(p.data.slug);
+    return fileEnabled ?? envEnabled;
+  });
+
+  return activeProjects.map((p: CollectionEntry<'projects'>) => p.data);
+}
+
