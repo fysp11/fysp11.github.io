32d678384382934df9d632c3216a4833d7f8264f
Felipe Mendonça <fysp11@gmail.com>
2025-09-29 13:48:44 -0300

    chore: add GTM ID and cookie consent dependencies

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-export default {
-  plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},
-    '@tailwindcss/postcss': {},
-    autoprefixer: {}
-  }
-};
